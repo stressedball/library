02a2ca2510918b6b1abd6f6fc0b7225de6992a55
stressedball <tserchesi@gmail.com>
2022-11-06 17:22:14 +0100

    Add intercations to remove and add book to library

--- script.js
+++ script.js
@@ -8,11 +8,11 @@ const hiddenStyle = document.querySelectorAll('.hidden');
 const isRead = document.querySelector('input#isRead');
 const shelve = document.querySelector('.shelve-container');
 const title = document.querySelector('input#title');
-const tiles =  document.querySelectorAll('.tile-container');
 
 const book1 = new Book('Les misérables', 'Victor Hugo', 'read');
 const book2 = new Book('Moby Dick', 'Herman Melville', 'read');
 let library = [];
+
 
 //BIG BUTTON
 buttonBookSubmit.addEventListener('click', addBook);
@@ -28,12 +28,13 @@ function onLoad() {
     for (let button of removeButtons) {
         button.addEventListener('click', removeBook);
     }
+
 }
 
 function removeBook(event) {
-    console.log(event.target.classList)
-    // let tileToRemove = event.target.parentElement;
-    // tileToRemove.remove();
+    library.splice(event.target.dataset.index, 1);
+    let tileToRemove = event.target.parentElement;
+    tileToRemove.remove();
 }
 
 
@@ -55,6 +56,17 @@ function addBook() {
 
 function displayBooks() {
     for (let book of library) {
+        const tiles =  document.querySelectorAll('.tile-container');
+        if (library.indexOf(book) < tiles.length) {
+            if (tiles.length > 0) {
+                console.log('here', library.indexOf(book))
+                continue;
+            }
+        } 
+
+
+        //need to figure out a way to stop the loop from going through the library without printing again the first books
+        //for ex if last book is 'Les Misérables', DONT ADD ALL THE BOOKS FROM LES MISERABLES TO library [0]
         let tile = document.createElement('div');
         tile.classList.add('tile-container');
         shelve.appendChild(tile);
@@ -79,7 +91,9 @@ function displayBooks() {
         removeButton.setAttribute('data-index', `${library.indexOf(book)}`);
         removeButton.textContent = 'Remove';
         tile.appendChild(removeButton);
+
     }
+    
 
 }
 
